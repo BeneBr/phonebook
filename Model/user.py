ccbe88ed2e562a9a07f142396ceba94829b7f725
Gustavo Berned <gustavo.berned2@gmail.com>
2022-07-29 18:35:24 -0300

    Add contacts to the user

--- Model/user.py
+++ Model/user.py
@@ -1,19 +1,32 @@
 from Model.address import Address
-from typing import Optional
+from typing import Optional, List
 from dataclasses import dataclass
+
+
+class UserContacts:
+    pass
+
 
 @dataclass(init=True)
 class User:
-    avatar: str
     name: str
     last_name: str
     age: int
     phone_number: int
+    e_mail: str
     address: Optional[Address] = None
+    contacts = []
 
     @property
     def full_name(self) -> str:
         return self.name + " " + self.last_name
 
     def SetAddress(self, address: Address) -> None:
-        self.address = Address
+        self.address = address
+
+@dataclass
+class UserContacts:
+    user_contacts: Optional[List[User]] = None
+    
+    def GetUserContacts(self) -> List[User]:
+        return self.user_contacts
--- Tests/test_users.py
+++ Tests/test_users.py
@@ -5,7 +5,7 @@ from Model.address import Address
 @fixture
 def user() -> User:
     address = Address('Barros Cassal','Centro',280,'RS','Alegrete')
-    user = User('Joao','Neves',50,55999897979,address=address)
+    user = User(name='Joao',last_name='Neves',age=50,phone_number=55999897979 ,e_mail='gberned@gmail.com', address=address)
 
     return user
 
@@ -14,3 +14,17 @@ def testUser(user) -> None:
     assert user.name == 'Joao' and user.last_name == 'Neves'
     assert user.address.number == 280 and user.address.street == 'Barros Cassal'
     assert user.full_name == 'Joao Neves'
+    assert user.e_mail == 'gberned@gmail.com'
+    assert not user.contacts == 0
+
+def testUserContacts(user) -> None:
+    address = Address('Venancio Aires', 'Centro', 580, 'RS', 'Santa Maria')
+    user1 = User(name='Pedro', last_name='Cruzeira', age=75, phone_number=55999897979, e_mail='pedrocru@gmail.com',
+                address=address)
+    address2 = Address('Bento Manoel', 'Centro', 780, 'RS', 'Porto Alegre')
+    user2 = User(name='Ana', last_name='Julia', age=35, phone_number=55999897979, e_mail='anaJulia@gmail.com',
+                address=address2)
+    user.contacts.append(user1)
+    user.contacts.append(user2)
+
+    assert len(user.contacts) == 2
